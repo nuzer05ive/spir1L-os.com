e067f5c37334e9a9a018b554f1719da29e659fc6
nuzer05ive <143165695+nuzer05ive@users.noreply.github.com>
2025-06-23 07:51:03 -0700

    refine docs and config

--- firmware/flash.ts
+++ firmware/flash.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
+import path from 'path';
 
 export function flashFirmware(cfgPath: string) {
-  const cfg = fs.readFileSync(cfgPath, 'utf8');
+  const cfg = fs.readFileSync(path.resolve(cfgPath), 'utf8');
   console.log('Flashing firmware with config:\n' + cfg);
   // TODO: invoke hardware flashing toolchain
 }
