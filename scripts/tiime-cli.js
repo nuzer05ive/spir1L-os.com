975d1fc0ab1e1fa593764dc7e807f43364fb8059
nuzer05ive <nuzer05ive@gmail.com>
2025-06-23 11:14:48 -0700

    chore(ci): add stub tiime-cli for Netlify deploy step

--- scripts/tiime-cli.js
+++ scripts/tiime-cli.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
-// Stub CLI: just log & exit success until real deploy logic is added.
 console.log('[tiime-cli] (stub) Pretending to deploy:', process.argv.slice(2).join(' '));
 process.exit(0);
