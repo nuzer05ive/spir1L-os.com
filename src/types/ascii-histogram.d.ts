22fe236580df27d127c7b3c4735df258ee7624ea
nuzer05ive <nuzer05ive@gmail.com>
2025-06-23 10:29:28 -0700

    fix(ci): add ascii-histogram stub

--- src/types/ascii-histogram.d.ts
+++ src/types/ascii-histogram.d.ts
@@ -1,11 +1,1 @@
-declare module "ascii-histogram" {
-  export interface HistogramOptions {
-    bar?: string;
-    width?: number;
-  }
-  function histogram(
-    data: number[] | Record<string, number>,
-    options?: HistogramOptions
-  ): string;
-  export default histogram;
-}
+declare module 'ascii-histogram';
